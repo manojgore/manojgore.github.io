a0295cb1a5ee3d1abca4c4d8193e40c7eb6b234d
Manoj Gore <manojgore.mg@gmail.com>
2022-07-20 22:36:58 +0530

    Update experience.js

--- assets/js/experience.js
+++ assets/js/experience.js
@@ -20,6 +20,34 @@ const exp = [
   },
 ];
 
+const showCards2 = () => {
+  let output = "";
+  exp.forEach(
+    ({ title, cardImage, place, time, desp }) =>
+      (output += `        
+    <div class="col gaap" data-aos="fade-up" data-aos-easing="linear" data-aos-delay="100" data-aos-duration="400"> 
+      <div class="card card1">
+        <img src="${cardImage}" class="featured-image"/>
+        <article class="card-body">
+          <header>
+            <div class="title">
+              <h3>${title}</h3>
+            </div>
+            <p class="meta">
+              <span class="pre-heading">${place}</span><br>
+              <span class="author">${time}</span>
+            </p>
+            <ol>
+              ${desp}
+            </ol>
+          </header>
+        </article>
+      </div>
+    </div>
+      `)
+  );
+  experiencecards.innerHTML = output;
+};
 document.addEventListener("DOMContentLoaded", showCards2);
 
 const showCards = () => {
