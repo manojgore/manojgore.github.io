1ea580ccda539f93fbdb502c42fc0427baa6bf93
Manoj Gore <manojgore.mg@gmail.com>
2022-07-20 17:25:55 +0530

    Update experience.js

--- assets/js/experience.js
+++ assets/js/experience.js
@@ -7,9 +7,9 @@ const exp = [
   {
     title: "Software Developer",
     cardImage: "assets/images/experience-page/flipkart.jpg",
-    place: "Flipkart",
-    time: "(May, 2020 - present)",
-    desp: "<li>Worked to migrate Flipkart lite’s product page to AMP pages so that they load up instantly.</li> <li>Made changes in the current progressive web app of Flipkart to react to different actions performed on AMP page.</li> <li>Created a node module called “fk-amp” which abstracts all the files and middlewares and can be easily imported and used from express server.</li>",
+    place: "Krushi Darbar",
+    time: "(May, 2020 - August, 2021)",
+    desp: "<li>Work as Full Stack Developer</li> <li>It is an farming sector ferm</li> <li>Created a Android application</li>",
   },
   {
     title: "Student Developer",
