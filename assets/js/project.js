2bd134db878e1533f48b0ce18995d41b11bd7410
Manoj Gore <manojgore.mg@gmail.com>
2022-07-21 20:46:37 +0530

    Update project.js

--- assets/js/project.js
+++ assets/js/project.js
@@ -36,6 +36,7 @@ const showCards = () => {
               <div class="content">
               <div class="title-div">
                 <h1 class="title"><a href="#">${title}</a></h1>
+                <h3 class="description"><a href="#">${description}</a></h3>
                 </div>
             <ul class="menu-content"><br>
                   <li><a href="${Previewlink}" class="social-icon"><svg xmlns="http://www.w3.org/2000/svg" width="30" height="25" viewBox="0 0 30 28" fill="none" stroke="#fff" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" class="feather feather-monitor"><rect x="2" y="3" width="20" height="14" rx="2" ry="2"></rect><line x1="8" y1="21" x2="16" y2="21"></line><line x1="12" y1="17" x2="12" y2="21"></line></svg></a></li>
